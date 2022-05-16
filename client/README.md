b1b8cbb75b7347666a6757dc782c461021697ee8
Leon Handreke <leonh@ndreke.de>
2022-05-16 12:30:02 +0200

    Add little artnet client to set builtin effects

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# Höllenleuchten Client
+
+To switch builtin animations
+
+	python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+
+	./client.py --ip 192.168.0.101 1  # 1 is Rainbow
---
+++ client/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from stupidArtnet import StupidArtnet
+
+parser = argparse.ArgumentParser(description='Client to Höllenleuchten.')
+parser.add_argument('--ip', type=str, help="IP address of the node to send to.")
+parser.add_argument('effect', type=int, help="Effect to set")
+
+args = parser.parse_args()
+
+a = StupidArtnet(args.ip, universe=0)
+a.set_single_value(1, args.effect)
+a.show()
